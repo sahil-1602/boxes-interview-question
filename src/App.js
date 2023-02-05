42a788faf61e0edbb5bc382281c6dcd3f182ab95
sahil-1602 <sahildarji16@gmail.com>
2023-02-05 12:36:37 +0530

    the vanilla js way

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [noOfBoxesClicked, setNoOfBoxesClicked] = useState([]);
+  const noOfBoxes = 7;
+
+  const changeToGreen = (e) => {
+    if (
+      !e.target.classList.contains('apply-animation-green') &&
+      noOfBoxesClicked.length < noOfBoxes
+    ) {
+      e.target.classList.add('apply-animation-green');
+      setNoOfBoxesClicked(() => [...noOfBoxesClicked, e.target]);
+    }
+  };
+
+  const removeGreen = () => {
+    if (noOfBoxesClicked.length === noOfBoxes) {
+      for (let i = 0; i < noOfBoxesClicked.length; i++) {
+        let box = noOfBoxesClicked[i];
+        setTimeout(() => {
+          box.classList.remove('apply-animation-green');
+        }, i * 1000);
+      }
+    }
+    setNoOfBoxesClicked([]);
+  };
+
+  useEffect(() => {
+    if (noOfBoxesClicked.length === noOfBoxes) {
+      setTimeout(() => {
+        removeGreen();
+      }, 1500);
+    }
+  });
+
+  function getBoxes() {
+    const boxes = [0, 1, 2].map((i) => {
+      return (
+        <div key={`row row-${i + 1}`} className={`row row-${i + 1}`}>
+          {[0, 1, 2].map((j) => {
+            if (!(i === 1 && j > 0)) {
+              return (
+                <div
+                  onClick={changeToGreen}
+                  key={`row-${i + 1}-col-${j + 1} box`}
+                  className={`row-${i + 1}-col-${j + 1} box`}></div>
+              );
+            }
+            return undefined;
+          })}
+        </div>
+      );
+    });
+    return boxes;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>{getBoxes()}</div>
     </div>
   );
 }
